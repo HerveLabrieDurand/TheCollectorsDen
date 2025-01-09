4392b893661a61d404e3a0d4214fa03f3bce311f
Hervé Labrie-Durand <hervelabriedurand@gmail.com>
2025-01-09 13:36:41 -0500

    Initial Commit
    Empty Spring Boot (API) and React (Portals) projects

---
+++ TheCollectorsDenAPI/src/main/java/com/collectorsden/demo/TheCollectorsDenApiApplication.java
@@ -0,0 +1,13 @@
+package com.collectorsden.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCollectorsDenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCollectorsDenApiApplication.class, args);
+    }
+
+}
---
+++ TheCollectorsDenAPI/src/test/java/com/collectorsden/demo/TheCollectorsDenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectorsden.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCollectorsDenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
