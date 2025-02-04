fd2afb274342c93e169bbd989ac116e0e6c10f42
Hervé Labrie-Durand <hervelabriedurand@gmail.com>
2025-02-04 15:01:19 -0500

    Supress PrimeNg CSS parsing error when testing with jest

--- TheCollectorsDenPortal/src/setup-jest.ts
+++ TheCollectorsDenPortal/src/setup-jest.ts
@@ -1,3 +1,14 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
 setupZoneTestEnv();
+
+// Suppress specific CSS parsing errors from PrimeNG (or other dependencies)
+const originalConsoleError = console.error;
+
+console.error = (...args) => {
+  const errorMessage = args[0];
+  if (errorMessage?.type === 'css parsing') {
+    return;
+  }
+  originalConsoleError(...args);
+};
