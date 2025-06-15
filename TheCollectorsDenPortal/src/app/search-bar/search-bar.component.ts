7793c1a991117b33e6ad61cfece478ecc393c3a4
Hervé Labrie-Durand <hervelabriedurand@gmail.com>
2025-06-15 11:29:52 -0400

    Localize search bar input placeholder

--- TheCollectorsDenPortal/src/app/search-bar/search-bar.component.ts
+++ TheCollectorsDenPortal/src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { matSearch } from '@ng-icons/material-icons/baseline';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [NgIcon],
+  imports: [NgIcon, TranslatePipe],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
   viewProviders: [
